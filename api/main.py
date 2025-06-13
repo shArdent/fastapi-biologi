c0d73090f838586f15e1e186eafe75d275246a4c
shArdent <eartkuad5@gmail.com>
2025-06-13 11:16:29 +0700

    feat(articles): add article CRUD

--- api/main.py
+++ api/main.py
@@ -4,6 +4,7 @@ from api.routes import test
 from api.routes import predict 
 from api.routes import plant
 from api.routes import disease
+from api.routes import article
 
 api_router = APIRouter(prefix="/v1")
 
@@ -11,4 +12,5 @@ api_router.include_router(test.router)
 api_router.include_router(predict.router)
 api_router.include_router(plant.router)
 api_router.include_router(disease.router)
+api_router.include_router(article.router)
 
