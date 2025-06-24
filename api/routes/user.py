ceea16070ce256a9c38ff76ceeeffb4521fa693a
shArdent <eartkuad5@gmail.com>
2025-06-24 09:41:26 +0700

    refactor(user): remove unused import

--- api/routes/user.py
+++ api/routes/user.py
@@ -1,32 +1,18 @@
-from fastapi import APIRouter, HTTPException, Depends, status, Query
-from typing import Optional
+from fastapi import APIRouter, HTTPException, Depends, status 
 from google.cloud.exceptions import GoogleCloudError
 from google.cloud.firestore_v1 import (
-    Increment,
     SERVER_TIMESTAMP,
-    Query as FirestoreQuery,
 )
 from firebase_admin import auth
 
 from db.firestore import db
 from schemas.users import User
 from schemas.my_plants import (
-    MyPlantSummary,
-    MyPlantCreate,
-    PaginatedMyPlantSummary,
-    MyPlantUpdate,
-    SuccessUpdatePlant,
     SuccessResponse,
-    SuccessCreatePlant,
 )
 from utils.middlewares.verify_token import verify_firebase_token
-from utils.middlewares.verify_user_id_match import verify_user_id_match
 from constants.collection_name import (
     FIRESTORE_COLLECTION_USERS,
-    FIRESTORE_COLLECTION_MY_PLANTS,
-    FIRESTORE_COLLECTION_PLANTS,
-    FIRESTORE_COLLECTION_DISEASES,
-    FIRESTORE_DOCUMENT_METADATA,
 )
 
 router = APIRouter(prefix="/users", tags=["users"])
