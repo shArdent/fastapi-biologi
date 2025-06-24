b2df00d80992c812f32de5c7fd7748a0a6669087
shArdent <eartkuad5@gmail.com>
2025-06-24 09:40:00 +0700

    feat(article)!: remove article feature

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from api.routes import my_plant, predict, plant, disease, test, user, article
+from api.routes import my_plant, predict, plant, disease, test, user
 
 api_router = APIRouter(prefix="/v1")
 
@@ -8,6 +8,5 @@ api_router.include_router(test.router)
 api_router.include_router(predict.router)
 api_router.include_router(plant.router)
 api_router.include_router(disease.router)
-api_router.include_router(article.router)
 api_router.include_router(user.router)
 api_router.include_router(my_plant.router)
