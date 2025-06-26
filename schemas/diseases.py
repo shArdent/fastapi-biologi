70c6ce4912d9bd0d10861bf2cf2cefd04d19244c
shArdent <eartkuad5@gmail.com>
2025-06-26 09:37:31 +0700

    feat(diseases)!: change diseases schema

--- schemas/diseases.py
+++ schemas/diseases.py
@@ -1,22 +1,47 @@
 from pydantic import BaseModel
 from typing import List
 
+
 class RecoveryStep(BaseModel):
-    head: str
+    heading: str
     description: str
+
 
 class RecoveryCare(BaseModel):
     title: str
     step: List[RecoveryStep]
 
+
+class Treatments(BaseModel):
+    heading: str
+    items: List[str]
+
+
+class Preventions(BaseModel):
+    heading: str
+    items: List[str]
+
+
+class Causes(BaseModel):
+    heading: str
+    items: List[str]
+
+
+class Symptoms(BaseModel):
+    heading: str
+    items: List[str]
+
+
 class Diseases(BaseModel):
     name: str
     plants_listed: List[str]
     type: str
-    symptom: List[str]
-    prevention: List[str]
-    cause: List[str]
+    symptoms: Symptoms
+    preventions: Preventions
+    causes: Causes
+    treatments: Treatments
     recovery_care: List[RecoveryCare]
+
 
 class DiseasesPaginatedResponse(BaseModel):
     diseases: list[Diseases]
