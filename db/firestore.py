b1a823e354ffe993fb8ea1dc20a003adf467d2ba
shArdent <eartkuad5@gmail.com>
2025-07-04 08:28:04 +0700

    fix(auth): fix auth error invalid certificate

--- db/firestore.py
+++ db/firestore.py
@@ -3,8 +3,12 @@ from dotenv import load_dotenv
 
 import os
 
+import firebase_admin
+from firebase_admin import credentials
+
 from google.cloud.firestore_v1.async_client import AsyncClient
 from google.oauth2 import service_account
+from proto.message import Parse
 
 load_dotenv()
 
@@ -15,7 +19,10 @@ if cred_json is None:
 
 cred_dict = json.loads(cred_json)
 
-
 creds = service_account.Credentials.from_service_account_info(cred_dict)
+
+cert = credentials.Certificate(cred_dict)
+
+app = firebase_admin.initialize_app(cert)
 
 db = AsyncClient(credentials=creds, project=cred_dict["project_id"])
