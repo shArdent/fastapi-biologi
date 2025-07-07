f2446d7ded2c7050d4e768c7356383b39b01b649
shArdent <eartkuad5@gmail.com>
2025-07-07 17:24:18 +0700

    fix(plants): remove await on if block

--- api/routes/plant.py
+++ api/routes/plant.py
@@ -138,8 +138,6 @@ async def get_all_plants(
             if not plant_data:
                 continue
 
-            print(plant_data)
-
             response_data = {**plant_data, "id": doc.id}
             plants.append(PlantResponse(**response_data))
 
@@ -240,7 +238,7 @@ async def delete_plant(plant_id: str):
         plant_ref = db.collection(FIRESTORE_COLLECTION_PLANTS).document(plant_id)
         plant_doc = await plant_ref.get()
 
-        if not await plant_doc.exists:
+        if not plant_doc.exists:
             raise HTTPException(
                 status_code=404,
                 detail=f"Tanaman dengan nama {plant_id} tidak ditemukan",
