d9105429b5d4e6a05bfada2da5e6c8afd900ce49
shArdent <eartkuad5@gmail.com>
2025-07-15 20:24:58 +0700

    fix(signature): add admin verify middleware

--- api/routes/upload.py
+++ api/routes/upload.py
@@ -1,11 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from schemas.upload import AuthParams
 from utils.imagekit_client import imagekit
+from utils.middlewares.verify_is_admin import verify_is_admin
 
 router = APIRouter(prefix="/upload", tags=["upload"])
 
 
-@router.get("/signature", response_model=AuthParams)
+@router.get(
+    "/signature", response_model=AuthParams, dependencies=[Depends(verify_is_admin)]
+)
 def get_upload_signature():
     auth_params = imagekit.get_authentication_parameters()
     return auth_params
